661b5007c804d71611300753e1ee702b5e3390be
Yulduz_Oromova <mahmurbekovna@gmail.com>
2024-07-24 12:45:07 +0300

    map & rendering

---
+++ src/mock/student.js
@@ -0,0 +1,40 @@
+export const students = [
+  [
+    "O'qtuvchi",
+    [
+      { status: true, title: "Dars jadval" },
+      { status: false, title: "Oylik" },
+      { status: true, title: "Status" },
+      { status: true, title: "Reyting" },
+    ],
+  ],
+  [
+    "Director",
+    [
+      { status: true, title: "Dars jadval" },
+      { status: false, title: "Oylik" },
+      { status: true, title: "Status" },
+      { status: true, title: "Reyting" },
+    ],
+  ],
+  [
+    "Adminstrator",
+    [
+      { status: true, title: "Dars jadval" },
+      { status: false, title: "Oylik" },
+      { status: true, title: "Status" },
+      { status: true, title: "Reyting" },
+    ],
+  ],
+  [
+    "HR",
+    [
+      { status: true, title: "Dars jadval" },
+      { status: false, title: "Oylik" },
+      { status: true, title: "Status" },
+      { status: true, title: "Reyting" },
+    ]
+  ]
+]
+
+export default students
